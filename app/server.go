fb7f4ebfca211bcf6191b206a18fbe3361a5aff9
yugo-ibuki <y.ibuki91@gmail.com>
2023-01-08 01:49:33 +0900

    pass stage2

--- app/server.go
+++ app/server.go
@@ -14,9 +14,18 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	defer conn.Close()
+	buffer := make([]byte, 1024)
+	if _, err := conn.Read(buffer); err != nil {
+		fmt.Println("Error reading from buffer: ", err.Error())
+		os.Exit(1)
+	}
+
+	conn.Write([]byte("+PONG\r\n"))
 }
